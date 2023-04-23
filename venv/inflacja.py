11f31cff767b53c91bd9623bc57b2e7c4873895e
Peysone <peysone@MacBook-Pro-Peysone.local>
2023-04-23 23:15:49 +0200

    zaczynam logike

--- venv/inflacja.py
+++ venv/inflacja.py
@@ -13,19 +13,27 @@ Napisz program tak, by wysokość początkowego kredytu, oprocentowanie kredytu 
 i kwota raty były pobierane ze standardowego wejścia (terminal).
 
 """
-username = input("Witaj! Jest to aplikacja obliczająca wartość zadłużenia kredytowego." \
+username = input("Witaj! Jest to aplikacja obliczająca wartość zadłużenia kredytowego. " \
                 "Podaj proszę swoje imię by rozpocząć:   ")
-print(f"Drogi/a {name}! \n Podaj proszę wysokość początkową kredytu, oprocentowanie kredytu oraz kwotę raty.".format(name=username))
+print(f"Drogi/a {username}! \n Podaj proszę wysokość początkową kredytu, oprocentowanie kredytu oraz kwotę raty.")
 
+message = ("{month} \n Twoja pozostała kwota kredytu w miesiącu {month} to {amount}, \n to {less} mniej niż w poprzednim miesiącu.")
 
 start_amount = int(input('Teraz podaj wartość kredytu:  '))
-loan_interest = int(input('Następnie podaj wartość oprocentowania:  '))
-monthly_payment = int(input('Teraz podaj kwotę raty miesięcznej, na tej podstawię \n aplikacja obliczy pozostałą sumę do spłaty zadłużenia:  '))
-inflation
-amount = start_amount - (monthly_payment * loan_interest * inflation)
-message = ("Twoja pozostała kwota kredytu to {amount}, to {less} mniej niż w poprzednim miesiącu.")
+loan_interest = float(input('Następnie podaj wartość oprocentowania:  '))
+monthly_payment = float(input('Teraz podaj kwotę raty miesięcznej, \n na tej podstawię '\
+'aplikacja obliczy pozostałą sumę do spłaty zadłużenia:  '))
 
-print
+# Styczeń
+month = "styczeń"
+inflation = 0.02
+#amount logika
+# 
+amount =
+less= amount - start_amount
+
+print(message.format(month=month, amount=amount, less=less))
+
 
 
 
