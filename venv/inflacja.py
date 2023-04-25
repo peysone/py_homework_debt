e4f222f378d54d20a489fedbe554356c9462a7e2
Peysone <peysone@MacBook-Pro-Peysone.local>
2023-04-25 20:47:35 +0200

    dodany kolor do tekstu

--- venv/inflacja.py
+++ venv/inflacja.py
@@ -1,3 +1,5 @@
+import os
+
 """Stwórz program, który na podstawie
 
 tabeli inflacji wartości
@@ -24,14 +26,18 @@ loan_interest = float(input('Następnie podaj wartość oprocentowania:  '))
 monthly_payment = float(input('Teraz podaj kwotę raty miesięcznej, \n na tej podstawię '\
 'aplikacja obliczy pozostałą sumę do spłaty zadłużenia:  '))
 
-# Styczeń 0.02%
+print()
+print("\u001b[31;1m" + "ROK 2022")
+print("\u001b[33;1m")
+
+#styczeń 0.02%
 month = "styczeń"
 inflation = float(0.02)
 #amount logika
-# amount -> wysokość pożyczki minus (m-czna rata  * oprocentowanie )x inflacja
+# amount -> wysokość pożyczki minus (m-czna rata  * (m-czna rata * oprocentowanie )x inflacja)
 amount = start_amount - (monthly_payment + (monthly_payment * loan_interest) * inflation)
 #less logika
-#less -> początkowa kwota amount
+#less -> początkowa kwota - amount
 less = float(start_amount - amount)
 
 print(message.format(month=month, amount=amount, less=less))
@@ -40,7 +46,7 @@ print(message.format(month=month, amount=amount, less=less))
 month = "luty"
 inflation = float(0.00)
 #amount1 logika
-# amount1 -> amount minus (m-czna rata  + oprocentowanie )x inflacja
+# amount1 -> amount minus (m-czna rata  * (m-czna rata * oprocentowanie )x inflacja)
 amount1 = amount - (monthly_payment + (monthly_payment * loan_interest) * inflation)
 
 less1 = float(start_amount - amount1)
@@ -143,4 +149,125 @@ less11 = float(start_amount - amount11)
 
 print(message.format(month=month, amount=amount11, less=less11))
 print()
-print("ROK 2023")
+print ("\u001b[31;1m" + "ROK 2023")
+print("\u001b[33;1m")
+
+# Styczeń 0.02%
+month = "styczeń"
+inflation = float(0.02)
+#amount logika
+# amount -> wysokość pożyczki minus (m-czna rata  * (m-czna rata * oprocentowanie )x inflacja)
+amountA = amount11 - (monthly_payment + (monthly_payment * loan_interest) * inflation)
+#less logika
+#less -> początkowa kwota - amount
+lessA = float(start_amount - amountA)
+
+print(message.format(month=month, amount=amountA, less=lessA))
+
+#Luty 0%
+month = "luty"
+inflation = float(0.00)
+#amount1 logika
+# amount1 -> amount minus (m-czna rata  * (m-czna rata * oprocentowanie )x inflacja)
+amountB = amountA - (monthly_payment + (monthly_payment * loan_interest) * inflation)
+
+lessB = float(start_amount - amountB)
+
+print(message.format(month=month, amount=amountB, less=lessB))
+
+#Marzec 2 %
+month = "marzec"
+inflation = float(0.02)
+amountC = amountB - (monthly_payment + (monthly_payment * loan_interest) * inflation)
+
+lessC = float(start_amount - amountC)
+
+print(message.format(month=month, amount=amountC, less=lessC))
+
+#kwiecień 0%
+month = "kwiecień"
+inflation = float(0.01)
+amountD = amountC - (monthly_payment + (monthly_payment * loan_interest) * inflation)
+
+lessD = float(start_amount - amountD)
+
+print(message.format(month=month, amount=amountD, less=lessD))
+
+#maj 1 %
+month = "maj"
+inflation = float(0.02)
+amountE = amountD - (monthly_payment + (monthly_payment * loan_interest) * inflation)
+
+lessE = float(start_amount - amountE)
+
+print(message.format(month=month, amount=amountE, less=lessE))
+
+#czerwiec 1%
+
+month = "czerwiec"
+inflation = float(0.01)
+amountF = amountE - (monthly_payment + (monthly_payment * loan_interest) * inflation)
+
+lessF = float(start_amount - amountF)
+
+print(message.format(month=month, amount=amountF, less=lessF))
+
+# lipiec 1%
+
+month = "lipiec"
+inflation = float(0.01)
+amountG = amountF - (monthly_payment + (monthly_payment * loan_interest) * inflation)
+
+lessG = float(start_amount - amountG)
+
+print(message.format(month=month, amount=amountG, less=lessG))
+
+# sierpień 2%
+month = "sierpień"
+inflation = float(0.02)
+amountH = amountG - (monthly_payment + (monthly_payment * loan_interest) * inflation)
+
+lessH = float(start_amount - amountH)
+
+print(message.format(month=month, amount=amountH, less=lessH))
+
+# wrzesień 0%
+
+month = "wrzesień"
+inflation = float(0.00)
+amountI = amountH - (monthly_payment + (monthly_payment * loan_interest) * inflation)
+
+lessI = float(start_amount - amountI)
+#lessI wróć xd
+
+print(message.format(month=month, amount=amountI, less=lessI))
+
+# październik 1%
+
+month = "październik"
+inflation = float(0.01)
+amountJ = amountI - (monthly_payment + (monthly_payment * loan_interest) * inflation)
+
+lessJ = float(start_amount - amountJ)
+
+print(message.format(month=month, amount=amountJ, less=lessJ))
+
+# listopad 0%
+
+month = "listopad"
+inflation = float(0.00)
+amountK = amountJ - (monthly_payment + (monthly_payment * loan_interest) * inflation)
+
+lessK = float(start_amount - amountK)
+
+print(message.format(month=month, amount=amountK, less=lessK))
+
+# grudzień 1%
+
+month = "grudzień"
+inflation = float(0.01)
+amountL = amountK - (monthly_payment + (monthly_payment * loan_interest) * inflation)
+
+lessL = float(start_amount - amountL)
+
+print(message.format(month=month, amount=amountL, less=lessL))
